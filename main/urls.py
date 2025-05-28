ce398b59f41e587107b9a8e25fd2e04c000d2cf5
shamshod8052 <shamshodramazonov8052@gmail.com>
2025-05-28 15:27:43 +0500

    fix

--- main/urls.py
+++ main/urls.py
@@ -5,10 +5,13 @@ from django.urls import path
 from django.views.decorators.cache import never_cache
 from ckeditor_uploader import views as ckeditor_views
 
+from bot.views import process_update
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/upload/', ckeditor_views.upload, name='ckeditor_upload'),
     path('ckeditor/browse/', never_cache(ckeditor_views.browse), name='ckeditor_browse'),
+    path("webhook/<str:token>/", process_update, name="bot-process-updates"),
 ]
 
 if settings.DEBUG:
