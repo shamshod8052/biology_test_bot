d2122ba75d286ba6580165f2ad9caee2ec50bce8
shamshod8052 <shamshodramazonov8052@gmail.com>
2025-05-29 12:03:03 +0500

    fix

--- bot/functions/objects_to_text.py
+++ bot/functions/objects_to_text.py
@@ -8,4 +8,4 @@ def get_objects_text(queryset, atr_name, rows_num, page_num=1):
     for n, lesson in enumerate(page.object_list, 1 + (page.number - 1) * rows_num):
         text += f"\n<b>{n}.</b> {getattr(lesson, atr_name)}"
 
-    return text
+    return text.strip()
