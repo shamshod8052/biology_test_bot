2c4b4d25bccbb6ebfc6de6172657b6688d147cd2
shamshod8052 <shamshodramazonov8052@gmail.com>
2025-06-01 18:29:23 +0500

    change webhook settings

--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from django.views.decorators.cache import never_cache
-from ckeditor_uploader import views as ckeditor_views
-
-from bot.views import process_update
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ckeditor/upload/', ckeditor_views.upload, name='ckeditor_upload'),
-    path('ckeditor/browse/', never_cache(ckeditor_views.browse), name='ckeditor_browse'),
-    path("webhook/<str:token>/", process_update, name="bot-process-updates"),
+    path('bot/', include('bot.urls'))
 ]
 
 if settings.DEBUG:
