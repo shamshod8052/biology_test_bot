2ea7900fa066086f55c2b7dd2449aaf16fd122e6
shamshod8052 <shamshodramazonov8052@gmail.com>
2025-06-01 18:54:01 +0500

    admin sayt is ''

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('bot/', include('bot.urls'))
 ]
 
